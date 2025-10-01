c40e92e88ffbba23cf8e1832756387789a231a97
Dadaso <bandgardadaso47@gmail.com>
2025-10-01 21:43:10 +0530

    Initial commit - Bankai Next.js project

---
+++ components/header.jsx
@@ -0,0 +1,19 @@
+"use client"
+import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
+const Header = () => {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  )
+}
+
+export default Header
